50567dd779d8f7044dc5e1104d15682e1db88624
leeyeonju02 <85239317+leeyeonju02@users.noreply.github.com>
2025-05-08 23:22:07 +0900

    fix: props 수정

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -15,6 +15,9 @@ export default function TodoPage() {
     selectedNav,
     setSelectedNav,
     filteredTodos,
+    isDropdownVisible,
+    selectedTagColor,
+    setSelectedTagColor,
   } = useTodo();
 
   return (
@@ -31,6 +34,9 @@ export default function TodoPage() {
             todos={filteredTodos}
             onToggle={toggleComplete}
             onDelete={deleteTodo}
+            isDropdownVisible={isDropdownVisible}
+            selectedTagColor={selectedTagColor}
+            setSelectedTagColor={setSelectedTagColor}
           />
         )}
       </div>
